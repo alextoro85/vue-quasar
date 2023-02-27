15e8842f5aa6e8cff6778825366766abb0fb5e70
Alex Toro <alex.toro@smowltech.com>
2023-02-27 13:29:26 +0100

    lifecycle in components

--- 7_Vue_Components/app.js
+++ 7_Vue_Components/app.js
@@ -4,6 +4,7 @@ const app = Vue.createApp({
     },
     data() {
         return {
+            showButton: true,
         }
     },
 });
--- 7_Vue_Components/button.vue
+++ 7_Vue_Components/button.vue
@@ -4,11 +4,42 @@
 
 <script>
 export default {
-    props: ['label', 'color'],
+    props: ['label', 'color', 'log'],
     data() {
         return {
         }
-    }
+    },
+    beforeCreate() {
+        if (this.log)
+            console.log('beforeCreate', document.querySelector('#app').innerHTML)
+    },
+    created() {
+        if (this.log)
+            console.log('created()', document.querySelector('#app').innerHTML)
+    },
+    beforeMount() {
+        if (this.log)
+            console.log('beforeMount()', document.querySelector('#app').innerHTML)
+    },
+    mounted() {
+        if (this.log)
+            console.log('mounted()', document.querySelector('#app').innerHTML)
+    },
+    beforeUpdate() {
+        if (this.log)
+            console.log('beforeUpdate()', document.querySelector('#app').innerHTML)
+    },
+    updated() {
+        if (this.log)
+            console.log('updated()', document.querySelector('#app').innerHTML)
+    },
+    beforeUnmount() {
+        if (this.log)
+            console.log('beforeUnmount()', document.querySelector('#app').innerHTML)
+    },
+    unmounted() {
+        console.log('unmounted()', document.querySelector('#app').innerHTML)
+    },
 }
 </script>
 
@@ -16,7 +47,7 @@ export default {
 .btn {
     box-sizing: border-box;
     appearance: none;
-    background-color: transparent;
+    background-color: lightgrey;
     border: 2px solid v-bind('color');
     border-radius: 0.6em;
     color: v-bind('color');
